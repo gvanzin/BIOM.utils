9fccf1c6618580472cd9a2df27e985fc8469be73
Daniel Braithwaite <contact.dtb@gmail.com>
2014-02-26 22:56:07 -0600

    almost done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-An S3 Class for BIOM Format
+An S3 Class with Utilities, for the Biological Observation Matrix (BIOM) Format
 http://metagenomics.anl.gov
 http://biom-format.org
--- tests/all.R
+++ tests/all.R
@@ -2,3 +2,11 @@ yy <- RBIOM(call.MGRAST(...))
 summary(yy)
 dim(yy)
 length(yy)
+
+
+## if a RBIOM object gives trouble, get more info with:
+## is.BIOM(obj, check.all=TRUE)
+##
+## and maybe try:
+## fixed.obj <- is.RBIOM(obj, fix=TRUE)
+##
